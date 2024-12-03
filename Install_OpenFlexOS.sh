55506db6925cd29631579287d6c3d9d9288ce966
chriskevinlee <112503262+chriskevinlee@users.noreply.github.com>
2024-12-03 18:45:44 +0000

    Update Install_OpenFlexOS.sh

--- Install_OpenFlexOS.sh
+++ Install_OpenFlexOS.sh
@@ -71,7 +71,7 @@
         sudo unzip -d /usr/share/fonts /tmp/NerdFontsSymbolsOnly.zip
         sudo fc-cache -fv
 
-        apt install libconfig-dev libdbus-1-dev libegl-dev libev-dev libgl-dev libepoxy-dev libpcre2-dev libpixman-1-dev libx11-xcb-dev libxcb1-dev libxcb-composite0-dev libxcb-damage0-dev libxcb-dpms0-dev libxcb-glx0-dev libxcb-image0-dev libxcb-present-dev libxcb-randr0-dev libxcb-render0-dev libxcb-render-util0-dev libxcb-shape0-dev libxcb-util-dev libxcb-xfixes0-dev libxext-dev meson ninja-build uthash-dev
+        apt -y install libconfig-dev libdbus-1-dev libegl-dev libev-dev libgl-dev libepoxy-dev libpcre2-dev libpixman-1-dev libx11-xcb-dev libxcb1-dev libxcb-composite0-dev libxcb-damage0-dev libxcb-dpms0-dev libxcb-glx0-dev libxcb-image0-dev libxcb-present-dev libxcb-randr0-dev libxcb-render0-dev libxcb-render-util0-dev libxcb-shape0-dev libxcb-util-dev libxcb-xfixes0-dev libxext-dev meson ninja-build uthash-dev
         
         cd /tmp
         git clone https://github.com/FT-Labs/picom.git
